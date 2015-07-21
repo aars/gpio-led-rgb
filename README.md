ec9f23c8ddb6c93800cf392aa0436764f3cb801e
Aaron Heesakkers <aaronheesakkers@gmail.com>
2015-07-21 15:42:24 +0000

    Second commit. Im not telling.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Controls a simple RGB led via GPIO for raspberry pi.
+
+# Configfile.
+Define gpio pin numbers and default color in `~/.config/gpio-led-rgb/config.json`
--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 #!/usr/bin/node
 
 var _      = require('lodash');
+var fs     = require('fs');
 var LedRGB = require('./lib/led-rgb.js');
-var led = new LedRGB();
+
+// Our config, overrides via config file.
+var config = {
+	pins: {
+		red: 2,
+		green: 3,
+		blue: 4
+	},
+	defaultColor: 'white'
+}
+var configFile = process.env.HOME +'/.config/gpio-led-rgb/config.json';
+if (fs.existsSync(configFile)) {
+	try {
+		config = _.defaults(require(configFile), config);
+	} catch (e) {
+		console.error('Failed loading configuration file', configFile + "\r\n", e.message);
+		process.exit();
+	}
+}
 
 // Inputs.
 var command = process.argv[2];
 var option  = process.argv[3];
 
 // Available commands.
-var localCommands = ['on', 'off', 'exit'];
-var allCommands = _.values(led.colorNames).concat(localCommands);
+var localCommands = ['on', 'off', 'exit', 'config'];
+var allCommands = _.values(LedRGB.colorNames).concat(localCommands);
 
 // Do we know the command?
 if (allCommands.indexOf(command) == -1) {
-	return console.error('Unsupported command:', command);
+	console.log('Unsupported command:', command);
+	process.exit();
 }
+
+// Good to go!
+var led = new LedRGB.LedRGB(config.pins);
+
+// Exit will unexport GPIO pins on SIGINT.
+process.on('SIGINT', led.exit.bind(led));
 
 // Is it a LedRGB color? That's easy.
 if (localCommands.indexOf(command) == -1) {
@@ -23,10 +49,10 @@ if (localCommands.indexOf(command) == -1) {
 }
 
 var commands = {
-	exit: led.exit,
-	off: led.off
+	config: console.log.bind(console, 'Current configuration:', JSON.stringify(config, false, '  ')),
+	exit: led.exit.bind(led),
+	off: led.off.bind(led),
+	on: led.color.bind(led, config.defaultColor)
 };
 
 commands[command]();
-
-process.on('SIGINT', led.exit);
